b6667d4d70228d35d1a4be72b03e248e2afe5bf0
Irene Chebet <irenetowett201@gmail.com>
2025-11-23 23:59:05 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from .admin_view import admin_view  # import from admin_view.py
 
 urlpatterns = [
-    # Registration view stays function-based
-    path('register/', views.register_view, name='register'),
-
-    # Use Django's built-in class-based views for login/logout
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('admin-view/', admin_view, name='admin_view'),
 ]
 
