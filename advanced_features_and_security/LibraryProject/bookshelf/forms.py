64ed25c1631b072617f59edaf8e25ef1e173f0f2
Irene Chebet <irenetowett201@gmail.com>
2025-11-26 23:31:54 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,14 @@
-<form method="post">
-    {% csrf_token %}
-    <!-- form fields here -->
-    <input type="submit" value="Submit">
-</form>
+from django import forms
+from .models import Book
 
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
+
+    # Optional: additional validation
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if "<script>" in title:
+            raise forms.ValidationError("Invalid characters in title")
+        return title
