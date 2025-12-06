4021188cd46607867fa9f2e20db898f86a16a706
Irene Chebet <irenetowett201@gmail.com>
2025-12-06 17:49:24 +0300

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include  # include is required to link app urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # THIS connects your app's urls
+]
 
