99ac477be2b5aa5172adc4e9bdb8d1059dcb9278
Irene Chebet <irenetowett201@gmail.com>
2025-12-08 21:24:03 +0300

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -11,3 +11,17 @@ INSTALLED_APPS = [
     'api',
 ]
 
+INSTALLED_APPS = [
+    ...
+    'rest_framework',
+    'rest_framework.authtoken',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
